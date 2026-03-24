2085e795f5640798160102db9675adc1866ef56e
FuzzyLypt <larson.oliveira123@gmail.com>
2026-03-24 17:59:29 -0300

    Moved JOptionDialog to a single function, fixed system not closing when prompted to and added a "loading" window for when the algorithm is taking to long.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import javax.swing.JOptionPane;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
 
 boolean isPrime(int num) {
     if (num <= 1) return false;
@@ -14,12 +16,18 @@ boolean isPrime(int num) {
     return true;
 }
 
-int stringToInt(String str) {
+int stringToIntValidator(String str) {
     // Checks if the given value is a number.
-    try {
-        return Integer.parseInt(str);
-    } catch (NumberFormatException e) {
+    if (str == null) {
+        System.exit(0);
         return -1;
+    }
+    else {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
     }
 }
 
@@ -29,21 +37,49 @@ int[] userInput() {
     int countMax;
 
     String rangeMinStr = JOptionPane.showInputDialog("\nInsert a minimum range for prime number search: ");
-    rangeMin = stringToInt(rangeMinStr);
+    rangeMin = stringToIntValidator(rangeMinStr);
 
     String rangeMaxStr = JOptionPane.showInputDialog("\nInsert a maximum range for prime number search: ");
-    rangeMax = stringToInt(rangeMaxStr);
+    rangeMax = stringToIntValidator(rangeMaxStr);
 
     String countMaxStr = JOptionPane.showInputDialog("\nInsert the amount of prime numbers to search for: ");
-    countMax = stringToInt(countMaxStr) - 1;
+    countMax = stringToIntValidator(countMaxStr) - 1;
 
-    return new int[]{rangeMin, rangeMax, countMax};
+    return new int[] {rangeMin, rangeMax, countMax};
+}
+
+void menuOption(String optionString, String[] paneOptions) {
+    int menuOption = JOptionPane.showOptionDialog(null,
+            optionString,
+            "Prime Number Searcher v1",
+            JOptionPane.OK_CANCEL_OPTION,
+            JOptionPane.INFORMATION_MESSAGE,
+            null,
+            paneOptions,
+            0);
+    if (menuOption == JOptionPane.CANCEL_OPTION || menuOption == 1) {
+        System.exit(0);
+    }
 }
 
 void main() {
 
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+    String[] paneOptions = {"YES", "EXIT"};
 
+    String startMenuString = "Welcome to the Prime Number Searcher.\n\nDo you wish to proceed?";
+
+    String endMenuString = """
+                    The .txt file containing the numbers has been created inside the OUTPUT folder.
+                    
+                    Do you wish to continue?""";
+
+    String errorMenuString = """
+                    An error has ocurred and the output file could not be created.
+                    
+                    Do you wish to continue?""";
+
+    // Creates output folder so output files can be inserted there
     File outputFolder = new File("OUTPUT");
     if (!outputFolder.exists()) {
         boolean outputFolderExists = outputFolder.mkdirs();
@@ -53,35 +89,34 @@ void main() {
         }
     }
 
-    do {
-        int startMenuOption = JOptionPane.showOptionDialog(null,
-                "Welcome to the Prime Number Searcher.\n\nDo you wish to proceed?",
-                "Prime Number Searcher v1",
-                JOptionPane.OK_CANCEL_OPTION,
-                JOptionPane.INFORMATION_MESSAGE,
-                null,
-                null,
-                0);
-        if (startMenuOption == JOptionPane.CANCEL_OPTION || startMenuOption == JOptionPane.CLOSED_OPTION) {
-            System.exit(0);
-        }
+    menuOption(startMenuString, paneOptions);
 
+    do {
         // rangeMin = 0, rangeMax = 1, countMax = 2
         int[] input = userInput();
         if ((input[0] <=  0 && input[1] <= 0  && input[2] <= 0) || input[1] <= input[0]) {
             JOptionPane.showMessageDialog(null, "Error: Invalid operation.");
-            break;
+            menuOption(errorMenuString, paneOptions);
+            continue;
         }
 
+        // Output goes to .txt file, and the date is inserted into it's name so no file is overwritten.
         LocalDateTime currentDateTime = LocalDateTime.now();
         String dateString = currentDateTime.format(formatter);
-
         PrintWriter writer;
         try {
             writer = new PrintWriter("OUTPUT/prime_numbers" + dateString + ".txt", StandardCharsets.UTF_8);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        // Window to show that the calculation is being done
+        JDialog processingDialog = new JDialog();
+        processingDialog.setTitle("Processing");
+        processingDialog.add(new JLabel("  Processing...  "));
+        processingDialog.pack();
+        processingDialog.setLocationRelativeTo(null);
+        processingDialog.setVisible(true);
 
         // Searches for primes within the given range so the user can get the results.
         int count = 0;
@@ -95,7 +130,8 @@ void main() {
             ++input[0];
         }
         writer.close();
-
+        processingDialog.dispose();
+        menuOption(endMenuString, paneOptions);
     } while (true);
 
 }
